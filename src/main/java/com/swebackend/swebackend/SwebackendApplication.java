4b813e185da21a5584b8fdab7d7de854e97e63b7
Reyes Blasco Cuadrado <reyesblascocuadrado@gmail.com>
2026-01-09 17:27:42 +0100

    First commit: Inicialización de proyecto

---
+++ src/main/java/com/swebackend/swebackend/SwebackendApplication.java
@@ -0,0 +1,13 @@
+package com.swebackend.swebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swebackend/swebackend/SwebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swebackend.swebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
