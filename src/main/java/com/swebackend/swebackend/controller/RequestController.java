e70220799151becd0ee8ee9d5cee5ebc1b73f777
Reyes Blasco Cuadrado <reyesblascocuadrado@gmail.com>
2026-01-11 18:05:22 +0100

    RF-2: Implementado cambio de estado
    Implementado endpoint de cambio de estado
    Controlados los casos de cambio de estado:
    Flujo de estados: Pendiente ->
    Aprobada o Rechazada, Aprobada -> Cancelada

--- src/main/java/com/swebackend/swebackend/controller/RequestController.java
+++ src/main/java/com/swebackend/swebackend/controller/RequestController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.swebackend.swebackend.model.Request;
+import com.swebackend.swebackend.model.RequestState;
 import com.swebackend.swebackend.service.RequestService;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -23,6 +26,12 @@ public class RequestController {
     @PostMapping
     public ResponseEntity<Request> create(@RequestBody Request request) {        
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(request));
+    }
+
+    @PatchMapping("/{id}/state")
+    public ResponseEntity<Request> changeState(@PathVariable Long id, @RequestBody String newState) {
+        Request updatedRequest = service.changeState(id, RequestState.valueOf(newState));
+        return ResponseEntity.ok(updatedRequest);
     }
 
 }
